ef6dba33186c3cc3a252a85fbbc02fe9514b9469
victor conqueror <victorvector608@gmail.com>
2025-05-13 13:33:34 +0100

    feat:added auth to the route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { login } = require('../controller/adminAuth');
+const { adminAuth } = require('../middleware/AuthMiddleware');
 const adminRouter = express.Router();
 
-adminRouter.post("/login", login);
+adminRouter.post("/login",adminAuth, login);
 
 module.exports = adminRouter;
